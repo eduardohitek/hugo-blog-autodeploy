9a17c9ef8c008d383ae77da648bbff4e6f1740b8
Eduardo Hitek <eduardohitek@gmail.com>
2019-06-13 19:55:39 -0300

    Adicionando PushWebHook Struck

--- server.go
+++ server.go
@@ -10,6 +10,29 @@ import (
 	"time"
 )
 
+//PushWebHook Struct que representa uma chamda do GithubWebhook
+type PushWebHook struct {
+	Action     string `json:"action" bson:"action"`
+	CheckSuite struct {
+		CreatedAt  string `json:"created_at" bson:"created_at"`
+		HeadBranch string `json:"head_branch" bson:"head_branch"`
+		HeadCommit struct {
+			Author struct {
+				Email string `json:"email" bson:"email"`
+				Name  string `json:"name" bson:"name"`
+			} `json:"author" bson:"author"`
+			ID        string `json:"id" bson:"id"`
+			Message   string `json:"message" bson:"message"`
+			Timestamp string `json:"timestamp" bson:"timestamp"`
+			TreeID    string `json:"tree_id" bson:"tree_id"`
+		} `json:"head_commit" bson:"head_commit"`
+		HeadSha              string `json:"head_sha" bson:"head_sha"`
+		ID                   int64  `json:"id" bson:"id"`
+		LatestCheckRunsCount int64  `json:"latest_check_runs_count" bson:"latest_check_runs_count"`
+		NodeID               string `json:"node_id" bson:"node_id"`
+	} `json:"check_suite" bson:"check_suite"`
+}
+
 // AutoDeploy Handler for the Github Webhook
 func AutoDeploy(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -19,7 +42,7 @@ func AutoDeploy(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		request := Request{}
+		request := PushWebHook{}
 		log.Println(string(body))
 		json.Unmarshal(body, &request)
 		log.Println(request)
